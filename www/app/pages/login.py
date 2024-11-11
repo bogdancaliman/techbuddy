0fee0e62f33e15757b8fd800a5a4251bdb6fe999
Bogdan <bogdan.caliman112@gmail.com>
2024-11-11 18:44:32 +0200

    feat: added fastapi

--- www/app/pages/login.py
+++ www/app/pages/login.py
@@ -1,15 +1,54 @@
 import reflex as rx
 
 def login() -> rx.Component:
-    return rx.vstack(
-        rx.link(
-                rx.button("Go to reset password"),
-                href="/reset-password",
-                is_external=False,
+    return rx.container(
+        rx.flex(
+            rx.heading("Login"),
+            rx.box(                
+                rx.text("Email"),
+                rx.input(
+                    placeholder="john-doe@email.com",
+                    type="email",
+                    size="3",
+                ),
             ),
-        rx.link(
-                rx.button("Go to signup"),
-                href="/signup",
-                is_external=False,
-            )
+            rx.box(     
+                rx.text("Name"),
+                rx.input( 
+                    placeholder="John",
+                    type="text",
+                    size="3"
+
+                ),
+            ),
+            rx.box(
+                rx.text("Password"),
+                rx.input(
+                    placeholder="******",
+                    type="password",
+                    size="3"
+
+                ),
+            ),
+            rx.button("Login"),
+            spacing="4",
+            direction="column",
+            justify="center",
+            align="center",
+        ),
+        height="100vh",
     )
+    
+    
+# vstack(
+#         rx.link(
+#                 rx.button("Go to reset password"),
+#                 href="/reset-password",
+#                 is_external=False,
+#             ),
+#         rx.link(
+#                 rx.button("Go to signup"),
+#                 href="/signup",
+#                 is_external=False,
+#             )
+#     )
